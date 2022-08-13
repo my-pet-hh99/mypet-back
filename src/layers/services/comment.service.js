0c98717e6ee3b678e00dfb7be53a8138d5f1e197
jc <codeing999@gmail.com>
2022-08-13 15:18:45 +0900

    add  config.js, install dotenv

--- src/layers/services/comment.service.js
+++ src/layers/services/comment.service.js
@@ -2,4 +2,63 @@ const CommentRepository = require("../repositories/comment.repository");
 
 module.exports = class CommentService {
   commentRepository = new CommentRepository();
+
+  createComment = async (userId, postId, content) => {
+
+    try {
+
+      const isExistPost = await this.commentRepository.isExistPost()
+
+
+      
+    } catch(err){
+
+      console.log(err);
+      return res.status(400).json('댓글 작성이 실패하였습니다');
+
+    }
+  };
+
+  getComment = async (postId) => {
+
+    try {
+
+      
+      
+    } catch(err){
+
+      console.log(err);
+      return res.status(400).json('댓글 작성이 실패하였습니다');
+
+    }
+
+  };
+  updateComment = async (userId, commentId, content) => {
+
+    try {
+
+      
+      
+    } catch(err){
+
+      console.log(err);
+      return res.status(400).json('댓글 작성이 실패하였습니다');
+
+    }
+
+  };
+  deleteComment = async (userId, commentId) => {
+
+    try {
+
+      
+      
+    } catch(err){
+
+      console.log(err);
+      return res.status(400).json('댓글 작성이 실패하였습니다');
+
+    }
+
+  };
 };
