63749ebafd70fae8bd5b0e9b0dcfcc3d9117749a
jc <codeing999@gmail.com>
2022-08-15 10:15:45 +0900

    modify comment.repository.js

--- src/layers/repositories/comment.repository.js
+++ src/layers/repositories/comment.repository.js
@@ -2,6 +2,19 @@ const { Comment } = require("../../models");
 
 module.exports = class CommentRepository {
 
+    isExistComment = async (commentId) => {
+        try {
+            const result = await Comment.findByPk(commentId);
+            if (result === null) return null;
+            else return true;
+            //return result.dataValues;
+
+        } catch (err) {
+            console.log(err);
+            return { success : false, message : err.message};
+        }
+    };
+
     createComment = async (userId, postId, text) => {
         try {
             const result = await Comment.create({
@@ -21,7 +34,6 @@ module.exports = class CommentRepository {
     };
 
     getComment = async (postId) => {
-        console.log("dd");
         try {
             const result = await Comment.findAll( {
                 where : { postId : postId}
@@ -48,7 +60,7 @@ module.exports = class CommentRepository {
                 }
             });
             if (result[0] !== 1) throw new Error('댓글 수정에 실패하였습니다');
-            return { success : true, result : result };
+            return { success : true};
 
         } catch (err){
 
@@ -58,14 +70,13 @@ module.exports = class CommentRepository {
     }
 
     deleteComment = async (commentId) => {
-        console.log("dddd");
         try {
 
             const result = await Comment.destroy( {
 
                 where: { commentId : commentId}
               });
-            console.log(result);
+              
             if (result !== 1) throw new Error('댓글 삭제에 실패하였습니다');
             return { success : true, result : result };
 
