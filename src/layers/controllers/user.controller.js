a3bfac2790a4413c5fb7ac0332130793802f70d0
SoularOfDawn <tntkala001@gmail.com>
2022-08-15 11:05:15 +0900

    UserPart create login/logout/reIssue

--- src/layers/controllers/user.controller.js
+++ src/layers/controllers/user.controller.js
@@ -2,4 +2,27 @@ const UserService = require("../services/user.service");
 
 module.exports = class UserController {
   userService = new UserService();
+
+  login = async (req, res) => {
+    const { email, password } = req.body;
+
+    const response = await this.userService.login(email, password);
+
+    res
+      .status(response.status)
+      .json(
+        response.result,
+        response.result ? response.data : response.messege
+      );
+  };
+
+  logout = async (req, res) => {
+    const { sessionId } = req.body;
+  };
+
+  reIssue = async (req, res) => {
+    const { refreshToken, accessToken } = req.header.common;
+
+    const response = await this.userService;
+  };
 };
--- src/layers/repositories/user.repository.js
+++ src/layers/repositories/user.repository.js
@@ -1,3 +1,49 @@
-const { User } = require("../../models");
+const { User, Sessions } = require("../../models");
 
-module.exports = class UserRepository {};
+module.exports = class UserRepository {
+  findUserById = async (userId) => {
+    const user = await User.findByPk(userId);
+
+    return user;
+  };
+
+  findUserLogin = async (email, password) => {
+    const user = await User.findOne({
+      where: { email, password },
+    });
+
+    return user;
+  };
+
+  findUserByNN = async (nickname) => {
+    const user = await User.findOne({
+      where: { nickname },
+    });
+
+    return user;
+  };
+
+  createUser = async (email, nickname, password, answer) => {
+    const user = await User.create({ email, nickname, password, answer });
+
+    return user;
+  };
+
+  createSession = async (userId) => {
+    const session = await Sessions.create({ userId });
+
+    return session;
+  };
+
+  findSession = async (sessionId) => {
+    const session = await Sessions.findByPk(sessionId);
+
+    return session;
+  };
+
+  deleteSession = async (sessionId) => {
+    const success = await Sessions.destroy({ where: sessionId });
+
+    return success;
+  };
+};
