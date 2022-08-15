92f120add45d1a042d70bb90fa7c2f53948edf5e
alexander <dlgksqor9105@gmail.com>
2022-08-15 11:12:46 +0900

    회원가입

--- src/layers/repositories/user.repository.js
+++ src/layers/repositories/user.repository.js
@@ -1,3 +1,14 @@
 const { User } = require("../../models");
+class UserRepository {
+  createUsers = async (email, nickname, password) => {
+    const createUsersData = await Users.create({
+      email,
+      nickname,
+      password,
+    });
+
+    return createUsersData;
+  };
+}
 
 module.exports = class UserRepository {};
--- src/layers/services/user.service.js
+++ src/layers/services/user.service.js
@@ -1,5 +1,19 @@
 const UserRepository = require("../repositories/user.repository");
 
+class UserService {
+  usersRepository = new UsersRepository();
+
+  createUsers = async (email, nickname, password) => {
+    const userCreateData = await this.usersRepository.createUsers(
+      email,
+      nickname,
+      password
+    );
+
+    return userCreateData;
+  };
+}
+
 module.exports = class UserService {
   userRepository = new UserRepository();
 };
