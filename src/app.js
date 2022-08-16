922c68870d0a3bc8d02cc1b89571dcb1ccab6baa
jc <codeing999@gmail.com>
2022-08-17 08:59:18 +0900

    apply cors, modify comment

--- src/app.js
+++ src/app.js
@@ -4,19 +4,19 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 
-// const whitelist = ["http://127.0.0.1"];
-// const corsOptions = {
-//     origin : function (origin, callback) {
-//       if (whitelist.indexOf(origin) !== -1){
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not Allowed Origin!"));
-//       }
-//     }
-// };
+const whitelist = ["127.0.0.1:8080"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      console.log(origin);
+      callback(new Error("Not Allowed Origin!"));
+    }
+  },
+};
 
-// app.use(cors(corsOptions));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
