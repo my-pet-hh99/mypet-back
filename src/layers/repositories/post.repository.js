67277e7486f6a8e63c22ddcfbe44a67b1fdc9ab4
jc <codeing999@gmail.com>
2022-08-17 00:34:00 +0900

    임시

--- src/layers/repositories/post.repository.js
+++ src/layers/repositories/post.repository.js
@@ -2,54 +2,54 @@ const { Post } = require("../../models");
 const { User } = require("../../models");
 
 class PostRepository {
-  findAllPost = async (offset) => {
-    const posts = await Post.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ["email", "nickname"],
-        },
-      ],
-      limit: 3,
-      offset: offset,
-    });
 
-    return posts;
-  };
+    findAllPost = async (offset) => {
+        const posts = await Post.findAll({
+            include: [
+                {
+                  model: User,
+                  attributes: ['email', 'nickname'],
+                }
+             ],
+             limit:3,
+             offset:offset,
+        });
 
-  findPostById = async (postId) => {
-    const post = await Post.findByPk(postId, {
-      include: [
-        {
-          model: User,
-          attributes: ["email", "nickname", "userId"],
-        },
-      ],
-    });
+        return posts;
+    }
 
-    return post;
-  };
+    findPostById = async (postId) => {
+            const post = await Post.findByPk(postId,{
+                include: [
+                    {
+                      model: User,
+                      attributes: ['email', 'nickname', 'userId'],
+                    }
+                 ],
+            });
 
-  createPost = async (userId, imageUrl, text) => {
-    const createPostData = await Post.create({
-      userId,
-      imageUrl,
-      text,
-    });
-  };
+            return post;
+      };
 
-  updatePost = async (postId, imageUrl, text) => {
-    const updatePostData = Post.update(
-      {
-        imageUrl: imageUrl,
-        text: text,
-      },
-      {
-        where: { postId },
-      }
-    );
-    return updatePostData;
-  };
+    createPost = async (userId, imageUrl,text) => {
+        const createPostData = await Post.create({
+            userId,
+            imageUrl,
+            text,
+        });
+        return updatePostData;
+    };
+
+    updatePost = async (postId, imageUrl, text) => {
+        const updatePostData = Post.update({
+            imageUrl: imageUrl,
+            text: text,
+            }, {
+                where: {postId}
+            });
+        return updatePostData;
+    };
+
   deletePost = async (postId) => {
     const deletePostData = Post.destroy({
       where: { postId },
