d66dd474930391a3778a887b0fc196dbef0c4fba
SoularOfDawn <tntkala001@gmail.com>
2022-08-16 13:39:50 +0900

    UserPart Fix ReIssue -

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const { Access } = require("../config/secretKey");
 
 module.exports = (req, res, next) => {
-  const token = req.headers.authorization;
+  const { accessToken } = req.headers.authorization;
 
   try {
     const tokenValue = jwt.verify(accessToken, Access.Secret);
