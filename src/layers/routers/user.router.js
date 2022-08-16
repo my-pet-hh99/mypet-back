2b97b18f089a318376487b9041e804a5498adc8a
SoularOfDawn <tntkala001@gmail.com>
2022-08-16 23:53:42 +0900

    UserPart Apply hasToken middleware

--- src/layers/routers/user.router.js
+++ src/layers/routers/user.router.js
@@ -6,15 +6,16 @@ const userController = new UserController();
 
 const needRefresh = require("../../middlewares/refresh.middleware");
 const auth = require("../../middlewares/auth.middleware");
+const hasToken = require("../../middlewares/hasToken.middleware");
 
 // http://localhost:3000/api/user]
 
 // 회원가입
-router.post("/signup", userController.signup);
-router.get("/idCheck", userController.checkEmail);
+router.post("/signup", hasToken, userController.signup);
+router.get("/idCheck", hasToken, userController.checkEmail);
 
 // 로그인 & 로그아웃 & 토큰 재발급
-router.post("/login", userController.login);
+router.post("/login", hasToken, userController.login);
 router.post("/token", needRefresh, userController.reIssue);
 router.delete("/logout", needRefresh, userController.logout);
 
